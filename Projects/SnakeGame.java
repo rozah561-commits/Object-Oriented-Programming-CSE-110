b64f0cc879ff01a8ea65345e3a3e0da299301491
Zannatul Hasan Roza <rozah561@gmail.com>
2026-06-05 23:19:25 +0600

    Add files via upload

---
+++ Projects/SnakeGame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package snakegame;
+
+import javax.swing.*;
+
+public class SnakeGame extends JFrame {
+    
+    SnakeGame() {
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new SnakeGame().setVisible(true);
+    }
+}
